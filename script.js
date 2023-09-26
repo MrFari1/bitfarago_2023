224c5c2575122c34af00012168011b55685312e0
kmate19 <99414737+kmate19@users.noreply.github.com>
2023-09-26 09:55:30 +0200

    e

--- script.js
+++ script.js
@@ -1,87 +1,154 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-setInterval(drawClock, 10);
-
-function drawClock() {
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
-  drawTime(ctx, radius);
+class Film{
+    constructor(Nev,Ev,Kategoria) {
+        this.nev=Nev
+        this.ev=Ev
+        this.Kategoria=Kategoria
+    }
+    pont=0;
 }
 
-function drawFace(ctx, radius) {
-  const grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
-  grad.addColorStop(0, '#000');
-  grad.addColorStop(0.5, '#6E6D6D'); //kulso kor
-  grad.addColorStop(1, '#333');
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2*Math.PI);
-  ctx.fillStyle = '#3A3A3A'; //belso resz
-  ctx.fill();
-  ctx.strokeStyle = grad;
-  ctx.lineWidth = radius*0.1;
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-  ctx.fillStyle = 'white'; //szamok
-  ctx.fill();
+const filmObj={
+    1:"The Shawshank Redemption;1994;dráma,bűnözés",
+    2:"Pulp Fiction;1994;dráma,bűnözés",
+    3:"Inception;2010;sci-fi,akció,thriller",
+    4:"The Godfather;1972;bűnözés,dráma",
+    5:"The Dark Knight;2008;akció,bűnözés,dráma",
+    6:"Forrest Gump;1994;dráma,romantikus",
+    7:"The Matrix;1999;sci-fi,akció",
+    8:"Jurassic Park;1993;sci-fi,kaland",
+    9:"Gladiator;2000;akció,dráma",
+    10:"The Lord of the Rings: The Fellowship of the Ring;2001;fantasztikus,kaland",
+    11:"Avatar;2009;sci-fi,kaland",
+    12:"Spirited Away;2001;animációs,fantasztikus,kaland",
+    13:"The Grand Budapest Hotel;2014;vígjáték,bűnözés,dráma",
+    14:"Black Panther;2018;sci-fi,akció,kaland",
+    15:"La La Land;2016;musical,dráma,romantikus"
 }
 
-function drawNumbers(ctx, radius) {
-  ctx.font = radius*0.15 + "px arial";
-  ctx.textBaseline="middle";
-  ctx.textAlign="center";
-  for(let num = 1; num < 13; num++){
-    let ang = num * Math.PI / 6;
-    ctx.rotate(ang);
-    ctx.translate(0, -radius*0.85);
-    ctx.rotate(-ang);
-    ctx.fillText(num.toString(), 0, 0);
-    ctx.rotate(ang);
-    ctx.translate(0, radius*0.85);
-    ctx.rotate(-ang);
-  }
+var jo=new Array();
+var nemjo=new Array();
+const filmek=new Array();
+
+for (let index = 1; index < 16; index++) {
+    let film=filmObj[index].split(';');
+    filmek.push(new Film(film[0],film[1],film[2].split(',')))
 }
 
-function drawTime(ctx, radius){
-    const now = new Date();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
-    //hour
-    hour=hour%12;
-    hour=(hour*Math.PI/6)+
-    (minute*Math.PI/(6*60))+
-    (second*Math.PI/(360*60));
-    drawHand(ctx, hour, radius*0.5, radius*0.07);
-    //minute
-    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-    drawHand(ctx, minute, radius*0.8, radius*0.07);
-    // second
-    second=(second*Math.PI/30);
-    drawHand(ctx, second, radius*0.9, radius*0.02);
+document.getElementsByTagName("input")[0].addEventListener("input",updateKereses);
+document.getElementsByTagName("input")[1].addEventListener("input",updateFilter);
+document.getElementById("search").addEventListener("click",mainKereses);
+
+
+for (let index = 0; index < 2; index++) {
+    document.getElementsByClassName("dropdown-menu")[index].appendChild
 }
 
-function drawHand(ctx, pos, length, width) {
-    ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.lineCap = "round";
-    ctx.moveTo(0,0);
-    ctx.rotate(pos);
-    ctx.lineTo(0, -length);
-    ctx.stroke();
-    ctx.rotate(-pos);
+function pontAdas(category) {
+    filmek.forEach(film => {
+        film.Kategoria.forEach(kategori=>{
+            if (kategori==category) {
+                film.pont++;
+            }
+        })
+    });
 }
 
-function popup(button) {
-  let id=String(button.id)
-  id=id.slice(5)
-  var popup=document.getElementById(id)
-  popup.classList.toggle("show");
+function pontLevonas(category) {
+    filmek.forEach(film => {
+        film.Kategoria.forEach(kategori=>{
+            if (kategori==category) {
+                film.pont--;
+            }
+        })
+    });
 }
 
-function imgshow(){
-  document.getElementById("btncheck1").checked==true ? document.getElementById("imageslider").hidden=false:document.getElementById("imageslider").hidden=true;
+
+function filteredSearch(){
+    let mode=document.querySelectorAll("input[type='checkbox'][id='jo']:checked")
+    mode.forEach(curr => {
+        if (curr.checked) {
+            pontAdas(curr.value)  
+        }
+    });
+    let mode2=document.querySelectorAll("input[type='checkbox'][id='rossz']:checked")
+    mode2.forEach(curr => {
+        if (curr.checked) {
+            pontLevonas(curr.value)  
+        }
+    });
 }
+
+
+function createFragment(img,nev,pont) {
+    let fragment=new DocumentFragment();
+    let div=document.createElement("div")
+    div.id="filmCucc"
+    pont>0 ? div.style.backgroundColor="green" : pont==0? div.style.backgroundColor="orange" :div.style.backgroundColor="red"
+    let kepdiv=document.createElement("div")
+    kepdiv.id="filmKep"
+    kepdiv.style.backgroundImage="url('kepek/"+img+".jpg"
+    let h2=document.createElement("h4")
+    h2.innerHTML=nev
+    div.appendChild(kepdiv)
+    div.appendChild(h2)
+    fragment.appendChild(div)
+    return fragment;
+}
+
+
+function mainKereses() {
+    filteredSearch()
+    filmek.sort((a,b) => b.pont - a.pont);
+    console.log(filmek);
+    filmek.forEach(film => {
+        document.getElementById("filmek").appendChild(createFragment(film.nev.split(' ').map(word => word.charAt(0).toLowerCase())
+        .join(''),film.nev,film.pont))
+    });
+}
+
+function updateKereses() {
+    console.log("called");
+    const kereses = document.getElementsByTagName("input")[0].value.toLowerCase();
+    const egyezes=filmek.filter(film=>film.nev.toLowerCase().includes(kereses));
+    if (egyezes.length==0) {
+        document.getElementById("filmek").innerHTML="{NINCS TALÁLAT"
+    } else {
+        egyezes.forEach(element => {
+            document.getElementById("filmek").innerHTML=element.nev
+        });
+    }
+}
+
+function updateFilter() {
+    document.getElementsByTagName("input")[1].checked==true ? document.getElementById("dropdowndiv").hidden=false : document.getElementById("dropdowndiv").hidden=true;
+}
+
+function loadFilter(){
+    let distinct = new Array();
+    let ul = document.getElementsByClassName("dropdown-menu");
+    filmek.forEach(film => {
+        film.Kategoria.forEach(item => {
+            if(!distinct.includes(item))
+            distinct.push(item)
+    });
+});
+for (let i = 0; i < ul.length; i++) {
+    for (let j = 0; j < distinct.length; j++) {
+        let label = document.createElement("label");
+        let checkbox = document.createElement("input");
+        let li = document.createElement("li");
+        i==0? checkbox.id="jo" : checkbox.id="rossz";
+        checkbox.type = "checkbox";
+        checkbox.value = distinct[j];
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(distinct[j]));
+        li.appendChild(label);
+        ul[i].appendChild(li);
+    }
+    console.log(distinct.length)
+}
+}
+document.addEventListener("load",loadFilter());
+
+
